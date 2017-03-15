e604f300386057a747daf8df3c11b32cb8a464fa
Bruno Gelatti <bgelatti@gmail.com>
2017-03-15 17:14:21 -0300

    correção no codigo sequencial

--- buddyadmin-application/src/main/java/br/com/codein/buddyadmin/application/service/CompanyService.java
+++ buddyadmin-application/src/main/java/br/com/codein/buddyadmin/application/service/CompanyService.java
@@ -87,11 +87,7 @@ public class CompanyService {
                 }
             });
         });
-        if (needToCreateSubOrganization(person)){
-            result = createSubOrganization(person, newOrganization);
-        } else {
-            result = securityClient.saveOrganization(newOrganization);
-        }
+        result = securityClient.saveOrganization(newOrganization);
 
         GumgaTenancyUtils.changeOi(result.getHierarchyCode(), person);
         personService.save(person);
@@ -155,34 +151,6 @@ public class CompanyService {
         } else {
             return null;
         }
-    }
-
-    @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
-    public Boolean needToCreateSubOrganization(Person p){
-
-        Boolean isRepresentative = p.containRoleWithCategory(RoleCategory.REPRESENTATIVE);
-        Boolean isSubCompany = Boolean.FALSE;
-        Boolean hasAggregatorFather = Boolean.FALSE;
-        if (p.getFather() != null){
-            Hibernate.initialize(p.getFather().getRoles());
-            isSubCompany = p.containRoleWithCategory(RoleCategory.COMPANY) &&
-                    p.getFather().containRoleWithCategory(RoleCategory.COMPANY);
-            hasAggregatorFather = p.getFather().containRoleWithCategory(RoleCategory.AGGREGATOR);
-        }
-       return isRepresentative || isSubCompany || hasAggregatorFather;
-    }
-
-    public Organization createSubOrganization(Person p, Organization org){
-        LinkedList<String> splicedFatherOi = new LinkedList<>(Arrays.asList(p.getFather().getOi().getValue().split("\\.")));
-        String rootFatherSecurityId = splicedFatherOi.removeFirst();
-        Organization root = this.getOrganization(Long.valueOf(rootFatherSecurityId));
-        Organization father = getChildRecursive(splicedFatherOi,root);
-        org.setMainUser(null);
-        father.getSubOrganizations().add(org);
-        securityClient.saveOrganization(root);
-        List<Organization> search = securityClient.getByInternalCode(org.getInternalCode());
-        return search.isEmpty()? null : search.get(0);
-
     }
 
     private Organization getChildRecursive(LinkedList<String>  splicedFatherOi, Organization org){
