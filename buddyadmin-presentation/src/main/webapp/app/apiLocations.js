4d3a71ee8d9045e4c4a25e0dba7bfbc6cfd1a9f1
portolan <hort_e@hotmail.com>
2018-02-09 15:47:11 -0200

    correção

--- buddyadmin-presentation/src/main/webapp/app/apiLocations.js
+++ buddyadmin-presentation/src/main/webapp/app/apiLocations.js
@@ -2,8 +2,7 @@
 define(function () {
     'use strict';
     return {
-        // apiLocation: location.origin+"/buddyadmin-api",
-        apiLocation: "http://localhost:8084/buddyadmin-api",
+        apiLocation: location.origin+"/buddyadmin-api",
         apiStorageLocation: 'http://45.79.202.166/storage-api',
         apiAmazonStorageLocation: 'https://hom-container-digital.s3-sa-east-1.amazonaws.com/',
         // apiFinanceLocation: location.protocol+'//'+location.hostname +':8084/finance-api',
