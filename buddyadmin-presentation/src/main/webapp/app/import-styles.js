ec077c0bfb9b2f4fa3de34a7341fe5adf66b3356
portolan <hort_e@hotmail.com>
2018-03-06 14:19:16 -0300

    correcao css

--- buddyadmin-presentation/src/main/webapp/app/import-styles.js
+++ buddyadmin-presentation/src/main/webapp/app/import-styles.js
@@ -1,7 +1,9 @@
 require("../node_modules/bootstrap/dist/css/bootstrap.min.css");
 require("../resources/styles/css/reset.css");
 require("../node_modules/animate.css/animate.min.css");
-require('../node_modules/components-font-awesome/css/fontawesome.min.css');
+// require('../node_modules/components-font-awesome/css/fontawesome.min.css');
+
+require('../bower_components/components-font-awesome/css/font-awesome.min.css');
 require('../bower_components/angular-ui-tree/dist/angular-ui-tree.min.css');
 require('../bower_components/ui-select/dist/select.min.css');
 require('../node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css');
