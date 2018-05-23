74d7bae3c632ce61f3441dec6745e006c01dfcaf
Felipe Sabadini Facina <fsabadini@hotmail.com>
2018-05-23 19:17:23 -0300

    correcoes na versao do framework e customresttemplate

---
+++ buddyadmin-infrastructure/src/main/java/br/com/codein/buddyadmin/infrastructure/config/http/BuddyadminGumgaRestTemplate.java
@@ -0,0 +1,32 @@
+package br.com.codein.buddyadmin.infrastructure.config.http;
+
+import io.gumga.presentation.CustomGumgaRestTemplate;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class BuddyadminGumgaRestTemplate implements CustomGumgaRestTemplate {
+
+    @Override
+    public HttpComponentsClientHttpRequestFactory getRequestFactory() {
+        CloseableHttpClient httpClient = HttpClients.custom().setUserAgent("buddyadmin-api").build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return requestFactory;
+    }
+
+    @Override
+    public List<HttpMessageConverter<?>> getMessageConverters() {
+        return Arrays.asList(new MappingJackson2HttpMessageConverter());
+    }
+}
